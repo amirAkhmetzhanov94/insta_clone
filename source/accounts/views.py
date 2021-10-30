8cf592282e08e952727b067809fd01da058d50b1
adlet_issatayev <issatayevadlet1@gmail.com>
2021-10-30 15:09:48 +0600

    Создал шаблон для регистрации, view для регистрации и путь

---
+++ source/accounts/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views.generic import View
+
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "login.html")
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        return render(request, "login.html", {"has_error": True})
--- source/instagram/urls.py
+++ source/instagram/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', accounts_views.LoginView.as_view(), name="login")
 ]
