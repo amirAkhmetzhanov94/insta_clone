77a95c09d32f2edde2db6f8ee454ef896440bb0f
Amir Akhmetzhanov <amir.akhmetzhanov94@gmail.com>
2021-11-05 01:30:04 +0600

    HW-62 Сделал вывод постов пользователя в профиле. Добавил счётчик постов и фолловеров на странице профиля. Подправил стилизацию формы добавления комментария.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -12,7 +12,10 @@ class PostForm(forms.ModelForm):
 
 class CommentForm(forms.ModelForm):
     text = forms.CharField(max_length=100, label="", widget=widgets.Input(attrs={"class": "mt-3",
-                                                                                 "placeholder": "Write your comment"}))
+                                                                                 "placeholder": "Write your comment",
+                                                                                 "style": "border: none; "
+                                                                                          "border-color: none; "
+                                                                                          "box-shadow: none;"}))
 
     class Meta:
         model = Comment
